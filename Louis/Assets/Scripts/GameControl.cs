40544774d3bef42187c015a16131fbcf1ab44bdf
Sébastien Ouellet <monster3d28@hotmail.com>
2014-05-23 17:14:04 -0400

    Update interaction and GUI

--- Louis/Assets/Scripts/GameControl.cs
+++ Louis/Assets/Scripts/GameControl.cs
@@ -3,13 +3,20 @@ using System.Collections;
 
 public class GameControl : MonoBehaviour {
 	bool toolOpened;
+	public Texture2D circle;
 
 	// Use this for initialization
 	void Start () {
+		Screen.lockCursor = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void OnGUI()
+	{
+		GUI.DrawTexture(new Rect((Screen.width - circle.width) / 2, (Screen.height - circle.height) /2, circle.width, circle.height),circle);
 	}
 }
---
+++ Louis/Assets/Scripts/ResourceBehavior.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResourceBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseOver()
+	{
+		Debug.Log ("Pablabla");
+		//transform.localScale += new Vector3 (2f,2f,2f);
+	}
+}
