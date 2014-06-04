2deb01d538dba64b5438f108f934cbaf285b1404
Sébastien Ouellet <monster3d28@hotmail.com>
2014-06-04 17:33:59 -0400

    Update interaction and eyetracking

---
+++ Louis/Assets/Scripts/Tracking.cs
@@ -0,0 +1,33 @@
+﻿/*
+using UnityEngine;
+using System.Collections;
+using Tobii
+
+public class Tracking : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		Tobii.EyeTracking.Sdk.Library.Init();
+		tracker = Tobii.EyeTracking.Sdk.EyetrackerFactory.CreateEyeTracker();
+		
+		//Todo: Calibration
+		
+		tracker.StartTracking();
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Debug.Log("some data");
+	}
+	
+	void OnApplicationQuit()
+	{
+		tracker.StopTracking();
+		//tracker.Dispose();
+	}
+}
+*/
