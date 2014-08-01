e9b570f0986d2e9078958ee0c23976d13a247fb2
Sébastien Ouellet <monster3d28@hotmail.com>
2014-08-01 16:56:55 -0400

    Text fields for self-evaluation and equations

---
+++ Louis/Assets/Scripts/EquationBalance.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EquationBalance : MonoBehaviour {
+
+	public bool display;
+	public string equation;   
+	public string answer;
+	
+	//sample: "\u2610 H + \u2610 O \u2192 3 H\u2082O"
+	
+	void Start()
+	{
+		display = false;
+		equation = "";
+		answer = "";
+	}
+		
+	void OnGUI()
+	{
+		if (display)
+		{
+			Screen.lockCursor = false;
+			GUI.skin.textField.fontSize = 30;
+			
+			Rect labelSize = GUILayoutUtility.GetRect (new GUIContent (equation), "textField");
+			equation = GUI.TextField (new Rect(Screen.width/2f-labelSize.width/2,40,labelSize.width,labelSize.height), equation);
+			
+			GUI.skin.button.fontSize = 16;
+			
+			if (GUI.Button (new Rect(Screen.width/2f-50, 90, 100, 40),"Envoyer"))
+			{
+				Screen.lockCursor = true;
+				answer = equation;
+				display = false;
+			}
+		}
+	}
+}
---
+++ Louis/Assets/Scripts/EvaluationTextfield.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EvaluationTextfield : MonoBehaviour {
+	public bool display;
+	public string textInput;
+
+	void Start()
+	{
+		display = false;
+		textInput = "Décris ici comment tu as résolu le probleme";
+	}
+
+	void OnGUI()
+	{
+		if (display)
+		{
+			Screen.lockCursor = false;
+
+			textInput = GUI.TextArea (new Rect(20,20,Screen.width-40,Screen.height/5f), textInput);
+			GUI.skin.button.fontSize = 16;
+			
+			if (GUI.Button (new Rect(Screen.width/2f-50, Screen.height/5f+30, 100, 40),"Envoyer"))
+			{
+				Screen.lockCursor = true;
+				textInput = "Décris ici comment tu as résolu le probleme";
+				display = false;
+			}
+		}
+	}
+	
+}
