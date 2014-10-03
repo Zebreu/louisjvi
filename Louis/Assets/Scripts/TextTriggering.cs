6d9c6c4e4e39b06a9c6f4b97287710ed32085e09
Sébastien Ouellet <monster3d28@hotmail.com>
2014-10-03 18:57:46 -0400

    Added a journal
    The journal compiles all text communicated to the player

--- Louis/Assets/Scripts/TextTriggering.cs
+++ Louis/Assets/Scripts/TextTriggering.cs
@@ -14,16 +14,29 @@ public class TextTriggering : MonoBehaviour {
 	string comm_filename = @"Assets/Scripts/TriggeredComm.xml";
 	string[] gameinfos;
 	string[] comms;
-	
+
+	bool journalShown;
+	string journal = "";
+
+	Vector2 scrollPosition;
+
 	public Texture2D orbital; 
 	
 	int stringStart = 14;
+
+	GUIStyle journalStyle;
 	
 	void Start()
 	// Reads in the files storing the text for the game hints and character communications
 	{
 		gameinfos = File.ReadAllText (gameinfo_filename).Split (new string[] {"</gameinfo>\r\n"}, System.StringSplitOptions.None);
 		comms = File.ReadAllText (comm_filename).Split (new string[] {"</communic>\r\n"}, System.StringSplitOptions.None);
+
+		journalShown = false;
+		journalStyle = new GUIStyle ();
+		journalStyle.fontSize = 16;
+		journalStyle.wordWrap = true;
+		journalStyle.normal.textColor = Color.white;
 	}
 	
 	public void GetTrigger(string triggerName)
@@ -34,7 +47,9 @@ public class TextTriggering : MonoBehaviour {
 		textID = TypeAndID[1];
 		display = FindText();
 	}
-	
+
+
+
 	bool FindText()
 	// Finds the right string to display depending on the box reached by the player
 	{
@@ -51,12 +66,23 @@ public class TextTriggering : MonoBehaviour {
 			if (item.Contains(textID))
 			{
 				text = item.Substring (stringStart+textID.Length+2);
+				journal += text+"\n\n";
+				scrollPosition = new Vector2(scrollPosition.x, Mathf.Infinity);
 				return true;
 			}
 		}
 		return false;		
 	}
-	
+
+	void showJournal()
+	{
+		GUILayout.BeginArea (new Rect (60, 5, 800, 200));
+		scrollPosition = GUILayout.BeginScrollView (scrollPosition, GUILayout.Width(800), GUILayout.Height(200));
+		GUILayout.Label (journal,journalStyle);
+		GUILayout.EndScrollView ();
+		GUILayout.EndArea ();
+	}
+
 	void Update()
 	// Allows the player to turn off the string displayed. Links IDs by their number (inventory1, inventory2, etc.) 
 	// as well as between character comms and game hints (comm_light1 to gameinfo_light1)
@@ -80,10 +106,27 @@ public class TextTriggering : MonoBehaviour {
 				display = FindText ();			
 			}
 		}
+		if (Input.GetButtonDown ("Show Journal"))
+		{
+			if (journalShown)
+			{
+				journalShown = false;
+			} else
+			{
+				journalShown = true;
+			}
+
+			Screen.lockCursor = !journalShown;
+			
+		}
 	}
 	
 	void OnGUI()
 	{
+		if (journalShown) 
+		{
+			showJournal ();
+		}
 		if (display)
 		{
 			GUI.skin.box.fontSize = 30;
