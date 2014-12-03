804fb5d1a5196e2896cccdb4e5f420d056d052f1
Sébastien Ouellet <monster3d28@hotmail.com>
2014-12-03 16:35:11 -0500

    Teleports back to start if player falls

---
+++ Louis/Assets/Scripts/Teleport.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleport : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnTriggerEnter (Collider col)
+	{
+		col.gameObject.transform.position = new Vector3(-45f,14f,68);
+	}
+}
